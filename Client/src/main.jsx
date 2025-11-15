8de2d61a3dd691cb4a1042b9e888e92c393e727d
Heet Shah <heet.shah123@spit.ac.in>
2025-11-15 13:12:06 +0530

    commits

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -6,3 +6,12 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
     <App />
 )
+
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+        navigator.serviceWorker
+            .register("/serviceworker.js")
+            .then(reg => console.log("Service worker registered:", reg.scope))
+            .catch(err => console.log("Service worker registration failed:", err));
+    });
+}
